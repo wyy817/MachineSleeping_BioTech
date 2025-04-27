d551d6691b25b205eb95c0326f2d9aa9d45d9aa8
Perry <161417981+NeoHyeo2k@users.noreply.github.com>
2025-04-27 08:27:42 +0800

    Delete MediFusion directory

--- MediFusion/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Choose the appropriate config for your environment
-app = create_app()
-
-if __name__ == "__main__":
-    # Start the Flask server
-    app.run(debug=True)  # Set debug=False for production
